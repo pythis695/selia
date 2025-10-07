f227511917fef93e7018b8e119f4f94f15c384d6
Nauval <nauvalazhar2@gmail.com>
2025-10-07 18:38:28 +0700

    chore: prerendering

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config';
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  async prerender() {
+    return ['/'];
+  },
 } satisfies Config;
